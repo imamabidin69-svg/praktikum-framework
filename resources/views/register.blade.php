150315220cd584a191af4426e0ddc70e997a981a
imamabidin999-maker <imamabidin999@gmail.com>
2026-06-08 12:41:14 +0700

    A

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,14 +1,26 @@
 <div style="width: 300px; margin: 50px auto; padding: 20px; border: 1px solid #ccc;">
     <h2>Register / Daftar Baru</h2>
+    
+    @if($errors->any())
+        <div style="color: red; margin-bottom: 15px; padding: 10px; border: 1px solid red; background-color: #ffe6e6;">
+            <strong>Registrasi Gagal:</strong>
+            <ul style="margin-top: 5px; padding-left: 20px;">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="/register" method="POST">
         @csrf
         <label>Nama Lengkap:</label><br>
-        <input type="text" name="name" required><br><br>
+        <input type="text" name="name" value="{{ old('name') }}" required><br><br>
         
         <label>Email:</label><br>
-        <input type="email" name="email" required><br><br>
+        <input type="email" name="email" value="{{ old('email') }}" required><br><br>
         
-        <label>Password:</label><br>
+        <label>Password (Min. 6 Karakter):</label><br>
         <input type="password" name="password" required><br><br>
         
         <button type="submit">Daftar</button>
